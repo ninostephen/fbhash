1d6481787b892d183222c4fa509a849ade91a442
Nino Stephen Mathew <16770117+ninostephen@users.noreply.github.com>
2020-05-20 11:50:10 +0530

    Fixed lost alignment
    Not sure how the whole alignment was lost after pushing it to the repo.

--- fbhash.py
+++ fbhash.py
@@ -30,8 +30,8 @@ dataSet.append("qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm")
 # Chunck calculator. Finds and returns a list of chunks in data object D with chunk length k.
 def chunkCalc(D, n):
 #	print("[+] Calculating chunks from the document... ", end="")
-	Ch = []						# Chunk list
-	k = 7 					    # Chunk length/Window Size
+	Ch = []			   # Chunk list
+	k = 7 			   # Chunk length/Window Size
 
 	for i in range(0, n - 6 ):  # For all elements from index 0 to ((n - 7) + 1)
 		Ch.append(D[i: (i + 7)])
@@ -56,10 +56,10 @@ def chunkCalc(D, n):
 # NB   : Lazy implementation. Too much computational time wasted!
 def rollHash(Ch):
 #	print("[+] Calculating RollingHash of ", len(Ch), " chunks... ", end="")
-	H = []   				# Hash list
-	a = 255  				# Constant
+	H = []   		# Hash list
+	a = 255  		# Constant
 	n = 801385653117583579  # Large prime number (larger than 2^56 = 72057594037927940)
-	h = 0	 				# Temporary variable
+	h = 0	 		# Temporary variable
 	#For every chunk in the chunk list, take one by one and find the rolling hash
 	for Ci in Ch: 
 		k = 7
@@ -80,7 +80,7 @@ def rollHash(Ch):
 # and the value associated with the key is the frequency of occurance of the chunk in D
 def chunkFrq(H):
 	print("[+] Calculating chunk frequency... ", end="")
-	ChfD = {}		# Dictionary of chunk frequency
+	ChfD = {}	# Dictionary of chunk frequency
 	uniq = set(H) 	# Filters out unique values from the list
 	
 	# For evert unique element, find its count
@@ -112,16 +112,16 @@ def chunkWgt(ChfD):
 # Check the frequency of hashes in H to the ones in the temporary list. 
 def docFrq(H):
 	print("[+] Calculating Document Frequency... ", end="")
-	h = [] 							# Temporary variable to hold the rolling hashes
-	dfCh = {}						# Document Frequency
-	global dataSet					# For referring to the global dataset generated.
+	h = [] 				   # Temporary variable to hold the rolling hashes
+	dfCh = {}			   # Document Frequency
+	global dataSet			   # For referring to the global dataset generated.
 	
-	for d in dataSet:				# Take every data object
-		Ch = chunkCalc(d, len(d))	# Find the chunks
-		h.extend(rollHash(Ch))		# Calculate the rolling hashes and put it into temporary variable
+	for d in dataSet:		   # Take every data object
+		Ch = chunkCalc(d, len(d))  # Find the chunks
+		h.extend(rollHash(Ch))	   # Calculate the rolling hashes and put it into temporary variable
 
-	for uniq in set(H):				# For all the unique hashes in list of the original document
-		dfCh[uniq] = h.count(uniq)	# Find the frequency of the chunk and store it int he dictionary
+	for uniq in set(H):		   # For all the unique hashes in list of the original document
+		dfCh[uniq] = h.count(uniq) # Find the frequency of the chunk and store it int he dictionary
 
 	print("Done")
 #	print("Document Frequency hash table : ", dfCh)
@@ -134,10 +134,10 @@ def docWgt(dfCh):
 	global N 			 	
 	idfCh = {}
 
-	for h in dfCh.keys(): 	# For every rolling hash in the hash table 
+	for h in dfCh.keys(): 		# For every rolling hash in the hash table 
 		if dfCh[h] > 0:		# If the frequency value is greater zero, only then proceed
 			idfCh[h] = log10(pow((N/dfCh[h]), 2))
-			continue		# Break the loop and start the next iteration
+			continue	# Break the loop and start the next iteration
 		idfCh[h] = 1		# Otherwise set the document weight to One
 	print("Done")
 #	print("Document Weight : ", idfCh)
@@ -149,7 +149,7 @@ def docWgt(dfCh):
 # Find the digest vector of all the documents
 def chunkScr(Chwgt, idfCh):
 	print("[+] Calculating Chunk Score... ", end="")
-	WDxCh = []					# Digest list
+	WDxCh = []			# Digest list
 
 	for key in Chwgt.keys():	# Since the keys of both the dictionaries are the same, we can use either
 		WDxCh.append(Chwgt[key] * idfCh[key])
@@ -161,25 +161,25 @@ def chunkScr(Chwgt, idfCh):
 # Calculate FbHash of document D
 def fbHash(D):
 	print("\n =============== Frequency based Hash Calculation ============== \n")
-	n      = len(D)	 				# Length of the data object/document
+	n      = len(D)	 			# Length of the data object/document
 	Ch     = chunkCalc(D, n)		# Chunks (Type : list)
 	H      = rollHash(Ch)			# Find and return the list of rolling hash of every chunk(Type : List)
 	ChfD   = chunkFrq(H)			# Hash table of chunk frequency (Type : Dict)
 	Chwgt  = chunkWgt(ChfD)			# Find the weight of each chunk w.r.t its frequency (Type : Dict)
-	dfCh   = docFrq(H)				# Find frequency of chunks in different Documents (Type : Dict)
+	dfCh   = docFrq(H)			# Find frequency of chunks in different Documents (Type : Dict)
 	idfCh  = docWgt(dfCh)			# Calculate the document weight (Type : Dict)
-	digest = chunkScr(Chwgt, idfCh)	# Calculate chunk score using chunk and document weight (Type : list)
+	digest = chunkScr(Chwgt, idfCh)		# Calculate chunk score using chunk and document weight (Type : list)
 	print("\n =============================== Done =========================== \n")
 
 	return digest
 
 # Similarity based Score Generation function.
-# 						    (Sum of all WD1Ci * WD2Ci where i = 0 to n-1)	 
+# 			    (Sum of all WD1Ci * WD2Ci where i = 0 to n-1)	 
 # Similarity(D1, D2) =  ----------------------------------------------------- * 100
-#						sqrt(Sum of all (WD1Ci)^2)*sqrt(Sum of all (WD2Ci)^2)
+#			sqrt(Sum of all (WD1Ci)^2)*sqrt(Sum of all (WD2Ci)^2)
 def smlrScrCalc(d1, d2):
 	print("[+] Calculating similiarity score... ", end="")
-	numerator 	 = 0
+	numerator    = 0
 	denomenator  = 0
 	denom1       = 0
 	denom2       = 0
@@ -207,7 +207,7 @@ def main():
 	d2 = fbHash(D2)
 
 # score is the similiarity scrore of the two vectors
-	score = smlrScrCalc	(d1, d2)
+	score = smlrScrCalc(d1, d2)
 	print("Similiarity score of D1 and D2 : ", round(score, 5), "%")
 	return 0
 
